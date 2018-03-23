a1230897998397521a63d902c2b032e572d5a7ae
Jacques <jacques@lsdev.biz>
2018-03-23 08:51:31 +0200

    Travis Fixes

--- includes/functions.php
+++ includes/functions.php
@@ -23,7 +23,7 @@ add_action( 'init', 'lsx_testimonials_load_plugin_textdomain' );
  */
 
 function lsx_testimonials( $args ) {
-	$lsx_testimonials = new LSX_Testimonials;
+	$lsx_testimonials = new LSX_Testimonials();
 	echo wp_kses_post( $lsx_testimonials->output( $args ) );
 }
 
@@ -31,7 +31,7 @@ function lsx_testimonials( $args ) {
  * Shortcode
  */
 function lsx_testimonials_shortcode( $atts ) {
-	$lsx_testimonials = new LSX_Testimonials;
+	$lsx_testimonials = new LSX_Testimonials();
 
 	return $lsx_testimonials->output( $atts );
 }
@@ -43,18 +43,18 @@ add_shortcode( 'lsx_testimonials', 'lsx_testimonials_shortcode' );
  */
 add_action( 'dashboard_glance_items', 'cpad_at_glance_content_table_end' );
 function cpad_at_glance_content_table_end() {
-	$args = array(
-		'public' => true,
+	$args     = array(
+		'public'   => true,
 		'_builtin' => false,
 	);
-	$output = 'object';
+	$output   = 'object';
 	$operator = 'and';
 
 	$post_types = get_post_types( $args, $output, $operator );
 	foreach ( $post_types as $post_type ) {
 		$num_posts = wp_count_posts( $post_type->name );
-		$num = number_format_i18n( $num_posts->publish );
-		$text = _n( $post_type->labels->singular_name, $post_type->labels->name, intval( $num_posts->publish ) );
+		$num       = number_format_i18n( $num_posts->publish );
+		$text      = _n( $post_type->labels->singular_name, $post_type->labels->name, intval( $num_posts->publish ) );
 		if ( current_user_can( 'edit_posts' ) ) {
 			$output = '<a href="edit.php?post_type=' . $post_type->name . '">' . $num . ' ' . $text . '</a>';
 			echo esc_html( '<li class="post-count ' . $post_type->name . '-count">' . $output . '</li>' );
