ef2d43000935f5d741a8889caa90fd9606c3b685
Warwick Booth <warwick@lsdev.biz>
2019-09-12 03:02:02 +0200

    Added in lazyloading for the sliders.

--- classes/class-lsx-testimonials-widget.php
+++ classes/class-lsx-testimonials-widget.php
@@ -94,7 +94,9 @@ class LSX_Testimonials_Widget extends WP_Widget {
 		if ( $tagline ) {
 			echo '<p class="tagline text-center">' . esc_html( $tagline ) . '</p>';
 		}
-
+		if ( 'true' === $carousel || true === $carousel ) {
+			add_filter( 'lsx_lazyload_slider_images', array( $this, 'lazyload_slider_images' ), 10, 5 );
+		}
 		if ( class_exists( 'LSX_Testimonials' ) ) {
 			lsx_testimonials( array(
 				'columns'    => $columns,
@@ -109,6 +111,9 @@ class LSX_Testimonials_Widget extends WP_Widget {
 				'carousel'   => $carousel,
 				'featured'   => $featured,
 			) );
+			if ( 'true' === $carousel || true === $carousel ) {
+				remove_filter( 'lsx_lazyload_slider_images', array( $this, 'lazyload_slider_images' ), 10, 5 );
+			}
 		}
 
 		if ( $button_text && $title_link ) {
@@ -320,6 +325,28 @@ class LSX_Testimonials_Widget extends WP_Widget {
 			<label for="<?php echo esc_attr( $this->get_field_id( 'col_tags' ) ); ?>"><?php esc_html_e( 'Enable Canonical Tags', 'lsx-testimonials' ); ?></label>
 		</p>
 		<?php
+	}
+
+	public function lazyload_slider_images( $img, $post_thumbnail_id, $size, $srcset, $image_url ) {
+		$lazyload = true;
+		if ( get_theme_mod( 'lsx_lazyload_status', '1' ) === false || ! apply_filters( 'lsx_lazyload_is_enabled', true ) ) {
+			$lazyload = false;
+		}
+		$lazy_img = '';
+		if ( true === $lazyload && '' !== $img ) {
+			$temp_lazy = wp_get_attachment_image_src( $post_thumbnail_id, $size );
+			if ( ! empty( $temp_lazy ) ) {
+				$lazy_img = $temp_lazy[0];
+			}
+			$img = '<img alt="' . the_title_attribute( 'echo=0' ) . '" class="attachment-responsive wp-post-image lsx-responsive" ';
+			if ( $srcset ) {
+				$img .= 'data-lazy="' . $lazy_img . '" srcset="' . esc_attr( $image_url ) . '" ';
+			} else {
+				$img .= 'data-lazy="' . esc_url( $image_url ) . '" ';
+			}
+			$img .= '/>';
+		}
+		return $img;
 	}
 }
 
