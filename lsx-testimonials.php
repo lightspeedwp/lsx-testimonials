9cb9dfbca08d877b549b8e7560b380b0cb233874
Ashley Shaw <ashley@lightspeedwp.agency>
2024-10-04 04:38:16 +0200

    Remove unused CSS and JavaScript files, added initial block files to register the templates, added initial block templates.

--- lsx-testimonials.php
+++ lsx-testimonials.php
@@ -1,44 +1,89 @@
 <?php
 /*
  * Plugin Name: LSX Testimonials
- * Plugin URI:  https://lsx.lsdev.biz/extensions/testimonials/
+ * Plugin URI:  https://lsx.design/product/lsx-testimonials/
  * Description: The LSX Testimonials extension adds the "Testimonials" post type.
- * Version:     1.3.6
+ * Version:     1.4
+ * Requires at least: 6.7-beta1
+ * Requires PHP:      8.0
  * Author:      LightSpeed
- * Author URI:  https://www.lsdev.biz/
+ * Author URI:  https://www.lightspeedwp.agency/
  * License:     GPL3
  * License URI: https://www.gnu.org/licenses/gpl-3.0.html
  * Text Domain: lsx-testimonials
  * Domain Path: /languages
  */
 
-// If this file is called directly, abort.
-if ( ! defined( 'WPINC' ) ) {
-	die;
-}
+
+ declare( strict_types = 1 );
 
 define( 'LSX_TESTIMONIALS_PATH', plugin_dir_path( __FILE__ ) );
 define( 'LSX_TESTIMONIALS_CORE', __FILE__ );
 define( 'LSX_TESTIMONIALS_URL', plugin_dir_url( __FILE__ ) );
-define( 'LSX_TESTIMONIALS_VER', '1.3.6' );
+define( 'LSX_TESTIMONIALS_VER', '1.4' );
 
 
-/* ======================= Below is the Plugin Class init ========================= */
+if ( ! function_exists( 'lsx_team_require_if_exists' ) ) {
+    /**
+     * Requires a file if it exists.
+     *
+     * @param string $file The file to require.
+     */
+    function lsx_team_require_if_exists( string $file ) {
+        if ( file_exists( $file ) ) {
+            require_once $file;
+        }
+    }
+}
 
-// Template Tag and functions.
-require_once LSX_TESTIMONIALS_PATH . '/includes/functions.php';
 
-// Post Type and Custom Fields.
-require_once LSX_TESTIMONIALS_PATH . '/classes/class-lsx-testimonials-admin.php';
+/**
+ * Register Team Post Type Archive:
+ * --------------------------------
+ * This plugin registers a custom block template for displaying team members and provides functionality to unregister the template.
+ *
+ * Functions:
+ * - lsx_team_register_template: Registers the custom block template using wp_register_block_template.
+ * - lsx_team_unregister_template: Unregisters the custom block template using wp_unregister_block_template.
+ *
+ * Hooks:
+ * - init: Calls lsx_team_register_template to register the template.
+ * - init: Calls lsx_team_unregister_template to unregister the template.
+ *
+ */
 
-// Frontend scripts and styles.
-require_once LSX_TESTIMONIALS_PATH . '/classes/class-lsx-testimonials-frontend.php';
+// Register the template
+add_action( 'init', 'lsx_testimonials_register_template' );
 
-// Shortcode and Template Tag.
-require_once LSX_TESTIMONIALS_PATH . '/classes/class-lsx-testimonials.php';
+function lsx_testimonials_register_template() {
+    // Add calls to wp_register_block_template() here.
+}
 
-// Widget.
-require_once LSX_TESTIMONIALS_PATH . '/classes/class-lsx-testimonials-widget.php';
+wp_register_block_template( 'lsx-testimonials/testimonials-archive', [
+	'title'       => __( 'LSX Testimonials Archive', 'lsx-testimonials' ),
+	'description' => __( 'Testimonials Archive template for displaying all testimonials with pagination.', 'lsx-testimonials' ),
+	'content'     => '
+	<!-- wp:template-part {"slug":"header","area":"header","tagName":"header"} /-->
+	<!-- wp:group {"tagName":"main"} -->
+	<main class="wp-block-group">
+		<!-- wp:group {"layout":{"type":"constrained"}} -->
+		<div class="wp-block-group">
+			<!-- wp:paragraph -->
+			<p>This is a plugin-registered template.</p>
+			<!-- /wp:paragraph -->
+		</div>
+		<!-- /wp:group -->
+	</main>
+	<!-- /wp:group -->
+	<!-- wp:template-part {"slug":"footer","area":"footer","tagName":"footer"} /-->',
+ ] );
+ 
 
-// Post reorder.
-require_once LSX_TESTIMONIALS_PATH . '/includes/class-lsx-testimonials-scpo-engine.php';
+// Deregister the template
+add_action( 'init', 'lsx_testimonials_unregister_template' );
+
+function lsx_testimonials_unregister_template() {
+    wp_unregister_block_template( 'lsx_testimonials//testimonials-archive' );
+}
+
+?>
